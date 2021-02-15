d18f7a17ab0ff537aec32624091a98864dd4ec85
1579376176 <1579376176@qq.com>
2021-02-15 17:40:15 +0800

    练习typescript

---
+++ TypeScript/Demo1.ts
@@ -0,0 +1,5 @@
+//类型注释
+const man:string = 'hanwen'
+const men:string[] = ["biyuanbo","chenjiyuan","dengyuxuan","cuisibo"]
+//类型推断
+const people = 'hanwen'  //把鼠标放在people上会发现类型是string,这一结果就是系统自动推断出来的
